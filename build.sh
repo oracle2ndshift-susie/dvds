074ea79cddb2eb4ef66ee5f050598bc707987106
oracle2ndshift-susie <oracle2ndshift@gmail.com>
2025-11-02 15:16:42 -0800

    init

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
 docrootread=`grep -i 'DocumentRoot' /etc/httpd/conf/httpd.conf | grep -v '#'  | head -n 1`
-docroot=`echo $docrootread | while read A B
+echo $docrootread | while read A B
 do 
   cd $B
   git clone --recursive https://github.com/oracle2ndshift-susie/dvds.git
   cd dvds
-  echo 'Enter database root password: '; read dbpwd
-  sed -i "s/Template_Password/${dbpwd}/g" build.sql
-  mysql -Dsys -u root -p  < build.sql
+  b1=`ls -1 | grep build.sh | wc -l`
+  b2=`ls -1 | grep build.sql | wc -l`
+  if [ $b1 != 1 || $b2 != 1 ]; then
+    echo "Something went wrong.  Please try again"
+  else
+    echo 'Enter database root password: '; read dbpwd
+    sed -i "s/Template_Password/${dbpwd}/g" build.sql
+    mysql -Dsys -u root -p  < build.sql
+  fi
 done
 
