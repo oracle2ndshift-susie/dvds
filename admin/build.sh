347ee4b294c0b0d76b6ea3ee714f7b0fc0715c0c
susie <susie@localhost.localdomain>
2025-11-02 11:45:18 -0800

    init

--- admin/build.sh
+++ admin/build.sh
@@ -1,10 +1,13 @@
-echo 'Enter directory of web site document root: '; read webdir
-cd $webdir
-echo "pwd = `pwd`"
-mkdir dvds_gif
-ls -ltr
+#!/bin/sh
 
-echo 'Enter database root password: '; read dbpwd
-mysql -Dsys -u root -p  < build.sql
-
+docrootread=`grep -i 'DocumentRoot' /etc/httpd/conf/httpd.conf | grep -v '#'  | head -n 1`
+docroot=`echo $docrootread | while read A B
+do 
+  cd $B
+  git clone --recursive https://github.com/oracle2ndshift-susie/dvds.git
+  cd dvds
+  echo 'Enter database root password: '; read dbpwd
+  sed -i "s/Template_Password/${dbpwd}/g" admin/build.sql
+  mysql -Dsys -u root -p  < admin/build.sql
+done
 
